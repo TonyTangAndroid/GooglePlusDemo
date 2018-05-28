5aa95c2835b94662885a85e38f6501400d04e11e
TonyTandAndroid <tonytangandroid@gmail.com>
2018-05-28 15:20:45 -0400

    Add Nested ViewPager support in DashboardFragment with TabLayout.

--- app/src/main/java/io/anycopy/googleplusdemo/DashboardFragment.java
+++ app/src/main/java/io/anycopy/googleplusdemo/DashboardFragment.java
@@ -3,12 +3,17 @@ package io.anycopy.googleplusdemo;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class DashboardFragment extends Fragment {
+
 
     public static DashboardFragment newInstance() {
         return new DashboardFragment();
@@ -19,4 +24,37 @@ public class DashboardFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_dashboard, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ViewPager viewPager = view.findViewById(R.id.view_pager);
+        viewPager.setAdapter(new GooglePlusFragmentPageAdapter(getChildFragmentManager()));
+        TabLayout tabLayout = view.findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private static class GooglePlusFragmentPageAdapter extends FragmentPagerAdapter {
+
+        public GooglePlusFragmentPageAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return DashboardChildFragment.newInstance(position);
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "Dashboard " + position;
+        }
+    }
+
 }
