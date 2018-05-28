4e34632bc51455e225611a17b9b1322b35da5e49
TonyTandAndroid <tonytangandroid@gmail.com>
2018-05-28 14:54:21 -0400

    Add ViewPager with Fragment Support.

---
+++ app/src/main/java/io/anycopy/googleplusdemo/DashboardFragment.java
@@ -0,0 +1,22 @@
+package io.anycopy.googleplusdemo;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DashboardFragment extends Fragment {
+
+    public static DashboardFragment newInstance() {
+        return new DashboardFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+    }
+}
---
+++ app/src/main/java/io/anycopy/googleplusdemo/NotificationFragment.java
@@ -0,0 +1,23 @@
+package io.anycopy.googleplusdemo;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NotificationFragment extends Fragment {
+
+
+    public static NotificationFragment newInstance() {
+        return new NotificationFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_notification, container, false);
+    }
+}
