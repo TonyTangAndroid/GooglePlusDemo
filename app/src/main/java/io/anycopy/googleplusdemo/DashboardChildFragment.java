245cf48f698d9197627553fd6720530f7b45f5db
TonyTang <tonythompsoncmu@gmail.com>
2018-12-18 22:20:51 -0500

    Androidx (#12)
    * upgrade to android x

--- app/src/main/java/io/anycopy/googleplusdemo/DashboardChildFragment.java
+++ app/src/main/java/io/anycopy/googleplusdemo/DashboardChildFragment.java
@@ -2,13 +2,14 @@ package io.anycopy.googleplusdemo;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 public class DashboardChildFragment extends Fragment {
 
--- app/src/main/java/io/anycopy/googleplusdemo/DashboardFragment.java
+++ app/src/main/java/io/anycopy/googleplusdemo/DashboardFragment.java
@@ -1,16 +1,18 @@
 package io.anycopy.googleplusdemo;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 public class DashboardFragment extends Fragment {
 
