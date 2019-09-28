d439e1b147d8dabf4ace82bb22abdb52a09f332b
TonyTandAndroid <tonytangandroid@gmail.com>
2019-09-28 13:35:55 -0700

    Add arguments to test navigation component.

--- app/src/main/java/io/anycopy/googleplusdemo/DashboardChildFragment.java
+++ app/src/main/java/io/anycopy/googleplusdemo/DashboardChildFragment.java
@@ -11,14 +11,18 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.Objects;
+
 public class DashboardChildFragment extends Fragment {
 
     private static final String ARGUMENT_POSITION = "argument_position";
+    private static final String ARGUMENT_NAME = "argument_name";
 
-    public static DashboardChildFragment newInstance(int position) {
+    public static DashboardChildFragment newInstance(int position, String name) {
 
         Bundle args = new Bundle();
         args.putInt(ARGUMENT_POSITION, position);
+        args.putString(ARGUMENT_NAME, name);
         DashboardChildFragment fragment = new DashboardChildFragment();
         fragment.setArguments(args);
         return fragment;
@@ -32,12 +36,16 @@ public class DashboardChildFragment extends Fragment {
 
 
     @SuppressLint("SetTextI18n")
-    @SuppressWarnings("ConstantConditions")
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         TextView tvDashBoard = view.findViewById(R.id.tv_dashboard);
-        int position = getArguments().getInt(ARGUMENT_POSITION, -1);
+        int position = Objects.requireNonNull(getArguments()).getInt(ARGUMENT_POSITION, -1);
+        String name = Objects.requireNonNull(getArguments()).getString(ARGUMENT_NAME);
         tvDashBoard.setText(position == 0 ? R.string.do_not_stop_believing : R.string.life_is_a_very_funny_proposition_after_all);
+        tvDashBoard.append("\n");
+        tvDashBoard.append("\n");
+        tvDashBoard.append("\n");
+        tvDashBoard.append(name);
     }
 }
--- app/src/main/java/io/anycopy/googleplusdemo/DashboardFragment.java
+++ app/src/main/java/io/anycopy/googleplusdemo/DashboardFragment.java
@@ -5,8 +5,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.material.tabs.TabLayout;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -14,11 +12,20 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.Objects;
+
 public class DashboardFragment extends Fragment {
 
+    private static final String ARG_NAME = "arg_name";
 
-    public static DashboardFragment newInstance() {
-        return new DashboardFragment();
+    public static DashboardFragment newInstance(String name) {
+        Bundle bundle = new Bundle();
+        bundle.putString(ARG_NAME, name);
+        DashboardFragment dashboardFragment = new DashboardFragment();
+        dashboardFragment.setArguments(bundle);
+        return dashboardFragment;
     }
 
     @Nullable
@@ -31,7 +38,8 @@ public class DashboardFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         ViewPager viewPager = view.findViewById(R.id.view_pager);
-        GooglePlusFragmentPageAdapter adapter = new GooglePlusFragmentPageAdapter(getChildFragmentManager());
+        GooglePlusFragmentPageAdapter adapter = new GooglePlusFragmentPageAdapter(getChildFragmentManager(),
+                Objects.requireNonNull(getArguments()).getString(ARG_NAME));
         viewPager.setAdapter(adapter);
         viewPager.setOffscreenPageLimit(adapter.getCount() - 1);
         TabLayout tabLayout = view.findViewById(R.id.tabs);
@@ -40,13 +48,16 @@ public class DashboardFragment extends Fragment {
 
     private static class GooglePlusFragmentPageAdapter extends FragmentPagerAdapter {
 
-        public GooglePlusFragmentPageAdapter(FragmentManager fm) {
-            super(fm);
+        private final String name;
+
+        public GooglePlusFragmentPageAdapter(FragmentManager fm, String name) {
+            super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+            this.name = name;
         }
 
         @Override
         public Fragment getItem(int position) {
-            return DashboardChildFragment.newInstance(position);
+            return DashboardChildFragment.newInstance(position, name);
         }
 
         @Override
