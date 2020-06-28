19b11aa698628aa458fa17aa5b3ef9ba03c81604
TonyTandAndroid <tonythompsoncmu@gmail.com>
2020-06-28 14:41:14 -0700

    Import Google Java Format

--- app/src/main/java/io/anycopy/googleplusdemo/BottomNavigationFABBehavior.java
+++ app/src/main/java/io/anycopy/googleplusdemo/BottomNavigationFABBehavior.java
@@ -4,40 +4,50 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
-public final class BottomNavigationFABBehavior extends CoordinatorLayout.Behavior<FloatingActionButton> {
-    public BottomNavigationFABBehavior(@Nullable Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-    }
+public final class BottomNavigationFABBehavior
+    extends CoordinatorLayout.Behavior<FloatingActionButton> {
 
-    public boolean layoutDependsOn(@Nullable CoordinatorLayout parent, @NonNull FloatingActionButton child, @NonNull View dependency) {
-        return dependency instanceof Snackbar.SnackbarLayout;
-    }
+  public BottomNavigationFABBehavior(@Nullable Context context, @Nullable AttributeSet attrs) {
+    super(context, attrs);
+  }
 
-    public void onDependentViewRemoved(@NonNull CoordinatorLayout parent, @NonNull FloatingActionButton child, @NonNull View dependency) {
-        child.setTranslationY(0.0f);
-    }
+  public boolean layoutDependsOn(
+      @Nullable CoordinatorLayout parent,
+      @NonNull FloatingActionButton child,
+      @NonNull View dependency) {
+    return dependency instanceof Snackbar.SnackbarLayout;
+  }
 
-    public boolean onDependentViewChanged(@NonNull CoordinatorLayout parent, @NonNull FloatingActionButton child, @NonNull View dependency) {
-        return this.updateButton(child, dependency);
-    }
+  public void onDependentViewRemoved(
+      @NonNull CoordinatorLayout parent,
+      @NonNull FloatingActionButton child,
+      @NonNull View dependency) {
+    child.setTranslationY(0.0f);
+  }
 
-    private boolean updateButton(View child, View dependency) {
-        if (dependency instanceof Snackbar.SnackbarLayout) {
-            float oldTranslation = child.getTranslationY();
-            float height = (float) dependency.getHeight();
-            float newTranslation = dependency.getTranslationY() - height;
-            child.setTranslationY(newTranslation);
-            return oldTranslation != newTranslation;
-        } else {
-            return false;
-        }
+  public boolean onDependentViewChanged(
+      @NonNull CoordinatorLayout parent,
+      @NonNull FloatingActionButton child,
+      @NonNull View dependency) {
+    return this.updateButton(child, dependency);
+  }
+
+  private boolean updateButton(View child, View dependency) {
+    if (dependency instanceof Snackbar.SnackbarLayout) {
+      float oldTranslation = child.getTranslationY();
+      float height = (float) dependency.getHeight();
+      float newTranslation = dependency.getTranslationY() - height;
+      child.setTranslationY(newTranslation);
+      return oldTranslation != newTranslation;
+    } else {
+      return false;
     }
+  }
 }
--- app/src/main/java/io/anycopy/googleplusdemo/MainActivity.java
+++ app/src/main/java/io/anycopy/googleplusdemo/MainActivity.java
@@ -22,154 +22,152 @@ import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ViewPager viewPager;
-    private BottomNavigationView navigation;
-    private Toolbar toolbar;
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+  private ViewPager viewPager;
+  private BottomNavigationView navigation;
+  private Toolbar toolbar;
+  private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener =
+      new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            toolbar.setTitle(item.getTitle());
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    viewPager.setCurrentItem(0);
-                    return true;
-                case R.id.navigation_dashboard:
-                    viewPager.setCurrentItem(1);
-                    return true;
-                case R.id.navigation_notifications:
-                    viewPager.setCurrentItem(2);
-                    return true;
-            }
-            return false;
+          toolbar.setTitle(item.getTitle());
+          switch (item.getItemId()) {
+            case R.id.navigation_home:
+              viewPager.setCurrentItem(0);
+              return true;
+            case R.id.navigation_dashboard:
+              viewPager.setCurrentItem(1);
+              return true;
+            case R.id.navigation_notifications:
+              viewPager.setCurrentItem(2);
+              return true;
+          }
+          return false;
         }
-    };
+      };
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.snack_bar, menu);
-        return super.onCreateOptionsMenu(menu);
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.snack_bar, menu);
+    return super.onCreateOptionsMenu(menu);
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case R.id.menu_show_snack_bar:
+        showSnackBar();
+        return true;
+      default:
+        return super.onOptionsItemSelected(item);
     }
+  }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.menu_show_snack_bar:
-                showSnackBar();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
+  private void showSnackBar() {
+    Snackbar.make(navigation, "Some text", Snackbar.LENGTH_LONG).show();
+  }
 
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    toolbar = findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+    viewPager = findViewById(R.id.view_pager);
+    GooglePlusFragmentPageAdapter adapter =
+        new GooglePlusFragmentPageAdapter(getSupportFragmentManager());
+    viewPager.setAdapter(adapter);
+    viewPager.setOffscreenPageLimit(adapter.getCount() - 1);
+    navigation = findViewById(R.id.navigation);
+    navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    bindNavigationDrawer();
+    initTitle();
+  }
 
-    private void showSnackBar() {
-        Snackbar.make(navigation, "Some text", Snackbar.LENGTH_LONG).show();
+  private void initTitle() {
+    toolbar.post(() -> toolbar.setTitle(navigation.getMenu().getItem(0).getTitle()));
+  }
 
-    }
+  private void bindNavigationDrawer() {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        viewPager = findViewById(R.id.view_pager);
-        GooglePlusFragmentPageAdapter adapter = new GooglePlusFragmentPageAdapter(getSupportFragmentManager());
-        viewPager.setAdapter(adapter);
-        viewPager.setOffscreenPageLimit(adapter.getCount() - 1);
-        navigation = findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-        bindNavigationDrawer();
-        initTitle();
-    }
+    DrawerLayout drawer = findViewById(R.id.drawer_layout);
+    ActionBarDrawerToggle toggle =
+        new ActionBarDrawerToggle(
+            this,
+            drawer,
+            toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close);
+    drawer.addDrawerListener(toggle);
+    toggle.syncState();
 
-    private void initTitle() {
-        toolbar.post(() -> toolbar.setTitle(navigation.getMenu().getItem(0).getTitle()));
-    }
-
-    private void bindNavigationDrawer() {
-
-
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                // Handle navigation view item clicks here.
-                int id = item.getItemId();
-                if (id == R.id.nav_tool) {
-                    showToolSnackBar();
-                } else if (id == R.id.nav_share) {
-                    showShareSnackBar();
-                } else if (id == R.id.nav_gallery) {
-                    showGallerySnackBar();
-                } else if (id == R.id.nav_send) {
-                    showSendSnackBar();
-                }
-                DrawerLayout drawer = findViewById(R.id.drawer_layout);
-                drawer.closeDrawer(GravityCompat.START);
-                return true;
+    NavigationView navigationView = findViewById(R.id.nav_view);
+    navigationView.setNavigationItemSelectedListener(
+        new NavigationView.OnNavigationItemSelectedListener() {
+          @Override
+          public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            // Handle navigation view item clicks here.
+            int id = item.getItemId();
+            if (id == R.id.nav_tool) {
+              showToolSnackBar();
+            } else if (id == R.id.nav_share) {
+              showShareSnackBar();
+            } else if (id == R.id.nav_gallery) {
+              showGallerySnackBar();
+            } else if (id == R.id.nav_send) {
+              showSendSnackBar();
             }
-
-
+            DrawerLayout drawer = findViewById(R.id.drawer_layout);
+            drawer.closeDrawer(GravityCompat.START);
+            return true;
+          }
         });
+  }
+
+  private void showSendSnackBar() {
+    Snackbar.make(navigation, "Send", Snackbar.LENGTH_SHORT).show();
+  }
+
+  private void showGallerySnackBar() {
+    Snackbar.make(navigation, "Gallery", Snackbar.LENGTH_SHORT).show();
+  }
+
+  private void showToolSnackBar() {
+    Snackbar.make(navigation, "Tool", Snackbar.LENGTH_SHORT).show();
+  }
+
+  private void showShareSnackBar() {
+    Snackbar.make(navigation, "Share", Snackbar.LENGTH_SHORT).show();
+  }
+
+  public void onFabClicked(View view) {
+    showShareSnackBar();
+  }
+
+  private static class GooglePlusFragmentPageAdapter extends FragmentPagerAdapter {
+
+    public GooglePlusFragmentPageAdapter(FragmentManager fm) {
+      super(fm);
     }
 
-    private void showSendSnackBar() {
-        Snackbar.make(navigation, "Send", Snackbar.LENGTH_SHORT).show();
-
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+      switch (position) {
+        case 0:
+          return HomeFragment.newInstance(1);
+        case 1:
+          return DashboardFragment.newInstance("Tony");
+        case 2:
+          return NotificationFragment.newInstance();
+        default:
+          throw new RuntimeException("Not supported");
+      }
     }
 
-    private void showGallerySnackBar() {
-        Snackbar.make(navigation, "Gallery", Snackbar.LENGTH_SHORT).show();
-
+    @Override
+    public int getCount() {
+      return 3;
     }
-
-    private void showToolSnackBar() {
-        Snackbar.make(navigation, "Tool", Snackbar.LENGTH_SHORT).show();
-    }
-
-    private void showShareSnackBar() {
-        Snackbar.make(navigation, "Share", Snackbar.LENGTH_SHORT).show();
-    }
-
-    public void onFabClicked(View view) {
-        showShareSnackBar();
-    }
-
-
-    private static class GooglePlusFragmentPageAdapter extends FragmentPagerAdapter {
-
-
-        public GooglePlusFragmentPageAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @NonNull
-        @Override
-        public Fragment getItem(int position) {
-            switch (position) {
-                case 0:
-                    return HomeFragment.newInstance(1);
-                case 1:
-                    return DashboardFragment.newInstance("Tony");
-                case 2:
-                    return NotificationFragment.newInstance();
-                default:
-                    throw new RuntimeException("Not supported");
-            }
-        }
-
-        @Override
-        public int getCount() {
-            return 3;
-        }
-    }
+  }
 }
