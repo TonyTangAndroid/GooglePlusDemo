4bbb6eb6246b4952cccd1cf57f503efb1d1fded1
TonyTandAndroid <tonythompsoncmu@gmail.com>
2020-06-28 15:08:23 -0700

    Split MainActivity responsibilities by introducing independent classes

---
+++ app/src/main/java/io/anycopy/googleplusdemo/BottomNavItemSelectedListener.java
@@ -0,0 +1,37 @@
+package io.anycopy.googleplusdemo;
+
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class BottomNavItemSelectedListener
+    implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+  private final Toolbar toolbar;
+  private final ViewPager viewPager;
+
+  public BottomNavItemSelectedListener(ViewPager viewPager, Toolbar toolbar) {
+    this.toolbar = toolbar;
+    this.viewPager = viewPager;
+  }
+
+  @Override
+  public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+    toolbar.setTitle(item.getTitle());
+    switch (item.getItemId()) {
+      case R.id.navigation_home:
+        viewPager.setCurrentItem(0);
+        return true;
+      case R.id.navigation_dashboard:
+        viewPager.setCurrentItem(1);
+        return true;
+      case R.id.navigation_notifications:
+        viewPager.setCurrentItem(2);
+        return true;
+      default:
+        return false;
+    }
+  }
+}
